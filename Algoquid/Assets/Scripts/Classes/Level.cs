b11d809319540f5f5da2c9b2abfdb71fd0990cf3
ThuleMalta <thulemalta@outlook.com>
2015-12-19 13:05:22 +0100

    Ajouts

---
+++ Algoquid/Assets/Scripts/Classes/Level.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+public class Level {
+	public string name { get; set; }
+	public string author { get; set; }
+	public string difficulty { get; set; }
+	public List<Element> elements { get; set; }
+
+	public class Position
+	{
+		public int x { get; set; }
+		public int z { get; set; }
+	}
+	
+	public class Element
+	{
+		public string name { get; set; }
+		public Position position { get; set; }
+		public int rotation { get; set; }
+	}
+}
---
+++ Algoquid/Assets/Scripts/Handlers/LevelLoadHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class LevelLoadHandler {
+
+	/// <summary>
+	/// Loads the level.
+	/// </summary>
+	/// <param name="json">Level JSON.</param>
+	public static void LoadLevel(string json) {
+
+	}
+
+}
