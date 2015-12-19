6b30e5a563941e4ab936d529441bb4bb6847683e
ThuleMalta <thulemalta@outlook.com>
2015-12-19 11:46:06 +0100

    Premier commit Création du projet Génération de la grille

---
+++ Algoquid/Assets/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿public class Constants {
+
+	//
+	// Game scene
+	//
+	public const string CAMERA_OBJ_NAME = "Main Camera";
+	public const string GRID_OBJ_NAME = "Grid";
+	public const int GRID_SIZE_X = 100;
+	public const int GRID_SIZE_Z = 100;
+	
+}
