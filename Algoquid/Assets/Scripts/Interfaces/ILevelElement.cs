e349edf053179c6425979d1325d8b1b56f47a348
ThuleMalta <thulemalta@outlook.com>
2015-12-20 15:21:13 +0100

    Ajout de paramètres dans l'interface `ILevelElement`

--- Algoquid/Assets/Scripts/Interfaces/ILevelElement.cs
+++ Algoquid/Assets/Scripts/Interfaces/ILevelElement.cs
@@ -1,5 +1,12 @@
-﻿public interface ILevelElement {
+﻿using UnityEngine;
+
+public interface ILevelElement {
+
 	// Element parameters
+	string name { get; set; }
+	GameObject model { get; set; }
+
+	// Booleans
 	bool interactable { get; set; }
 	bool pickable { get; set; }
 	bool breakable { get; set; }
